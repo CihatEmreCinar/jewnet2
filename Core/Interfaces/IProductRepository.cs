22771a86bab67decc5c2d3e7836c8b1c09ec9d20
CihatEmreCinar <cihatemre381@gmail.com>
2025-04-12 14:42:06 +0300

    2.commit

---
+++ Core/Interfaces/IProductRepository.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Reflection.Metadata;
+using Core.Entities;
+
+namespace Core.Interfaces;
+
+public interface IProductRepository
+{
+ Task<IReadOnlyList<Product>> GetProductsAsync();
+ Task<Product?> GetProductByIdAsync(int id);
+ void AddProduct(Product product);
+ void UpdateProduct(Product product);
+ void DeleteProduct(Product product);
+ bool ProductExist(int id);
+ Task<bool> SaveChangesAsync(); 
+ 
+}
