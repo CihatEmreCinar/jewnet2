76dc60d4e7ae053449bb590ef04083b18ba0afd1
CihatEmreCinar <cihatemre381@gmail.com>
2025-04-24 15:04:15 +0300

    SearchPartCommit

--- Core/Specification/ProductSpecification.cs
+++ Core/Specification/ProductSpecification.cs
@@ -3,27 +3,27 @@ using Core.Entities;
 
 namespace Core.Specification;
 
-public class ProductSpecification:BaseSpecification<Product>
+public class ProductSpecification : BaseSpecification<Product>
 {
-public ProductSpecification(ProductSpecParams specParams):base(x=>
-(specParams.Brands.Count ==0||specParams.Brands.Contains(x.Brand)) 
-&&
- (specParams.Types.Count ==0 ||specParams.Types.Contains(x.Type) )
-)
-{
-    ApplyPaging(specParams.PageSize*(specParams.PageSize -1), specParams.PageIndex);
-    switch (specParams.Sort)
+    public ProductSpecification(ProductSpecParams specParams) : base(x =>
+        (string.IsNullOrEmpty(specParams.Search) || x.Name.ToLower().Contains(specParams.Search)) &&
+        (specParams.Brands.Count == 0 || specParams.Brands.Contains(x.Brand)) &&
+        (specParams.Types.Count == 0 || specParams.Types.Contains(x.Type))
+    )
     {
-        case "priceAsc":
-            AddOrderBy(x=>x.Price);
-        break;
-         case "priceDesc":
-            AddOrderByDescending(x=>x.Price);
-        break;
-        default:
-            AddOrderBy(x=>x.Name);
-            break;
+        ApplyPaging(specParams.PageSize * (specParams.PageIndex -1), specParams.PageSize);
 
+        switch (specParams.Sort)
+        {
+            case "priceAsc":
+                AddOrderBy(x => x.Price);
+                break;
+            case "priceDesc":
+                AddOrderByDescending(x => x.Price);
+                break;
+            default:
+                AddOrderBy(x => x.Name);
+                break;
+        }
     }
-}
 }
