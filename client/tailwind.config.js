cc70e5f17d789a78017b1ae1ffa867c7ed717dcf
CihatEmreCinar <cihatemre381@gmail.com>
2025-04-30 17:33:28 +0300

    tailwind

---
+++ client/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // HTML ve TS dosyalarını tara
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
