8c0bde3f4114465dfa69ce04bcf291ec868946ad
CihatEmreCinar <cihatemre381@gmail.com>
2025-05-07 10:54:36 +0300

    Tail.windChanges

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { HeaderComponent } from './layout/header/header.component';
 import { RouterOutlet } from '@angular/router';
-
+ 
 @Component({
   selector: 'app-root',
-  standalone:true,
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet, HeaderComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Jewnet';
+  baseUrl = 'https://localhost:5001/api/';
+  private http = inject(HttpClient);
+  products: any[] = [];
+ 
+  ngOnInit(): void {
+    this.http.get<any>(this.baseUrl + 'products').subscribe({
+      next: response => this.products = response.data,
+      error: error => console.log(error),
+      complete: () => console.log('Request has completed')
+    })
+  }
 }
