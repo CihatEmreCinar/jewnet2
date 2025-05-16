f565ae020438ffaba72fd0c785593cff3b660086
CihatEmreCinar <cihatemre381@gmail.com>
2025-05-16 08:37:43 +0300

    TypeScript

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, inject, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { HeaderComponent } from './layout/header/header.component';
 import { RouterOutlet } from '@angular/router';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
  
 @Component({
   selector: 'app-root',
@@ -14,10 +16,10 @@ export class AppComponent implements OnInit {
   title = 'Jewnet';
   baseUrl = 'https://localhost:5001/api/';
   private http = inject(HttpClient);
-  products: any[] = [];
+  products: Product[] = [];
  
   ngOnInit(): void {
-    this.http.get<any>(this.baseUrl + 'products').subscribe({
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error),
       complete: () => console.log('Request has completed')
