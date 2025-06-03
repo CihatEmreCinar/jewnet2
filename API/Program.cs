48b9ddb47fddb7abed4c96f38e4d844207518a61
Adınız Soyadınız <email@example.com>
2025-06-03 17:37:33 +0300

    redis

--- API/Program.cs
+++ API/Program.cs
@@ -3,6 +3,7 @@ using Infrastructure.Data;
 using Core.Interfaces;
 using System.Threading.Tasks;
 using API.Middleware;
+using StackExchange.Redis;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -17,6 +18,13 @@ builder.Services.AddDbContext<StoreContext>(options =>
 builder.Services.AddScoped<IProductRepository, ProductRepository>();
 builder.Services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
 builder.Services.AddCors();
+builder.Services.AddSingleton<IConnectionMultiplexer>(config => 
+{
+    var connString = builder.Configuration.GetConnectionString("Redis") 
+        ?? throw new Exception("Cannot get redis connection string");
+    var configuration = ConfigurationOptions.Parse(connString, true);
+    return ConnectionMultiplexer.Connect(configuration);
+});
 var app = builder.Build();
 
 // Map Controllers (API Endpoints)
